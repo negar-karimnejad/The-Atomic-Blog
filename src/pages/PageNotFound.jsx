63d993fbd8bd0a1b6e2ab232161c9a3f40358649
negar <negar.karimnejad@gmail.com>
2024-03-03 15:41:44 +0330

    design not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="not-found">
+      <section className="error-container pt-28">
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+        <span className="zero">
+          <span className="screen-reader-text">0</span>
+        </span>
+        <span className="four">
+          <span className="screen-reader-text">4</span>
+        </span>
+      </section>
+      <div className="link-container">
+        <Link to="/" className="more-link">
+          Back to home
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
