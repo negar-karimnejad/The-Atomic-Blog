2a0229af0d9d32a38fa5620aa15c18d1da640385
negar <negar.karimnejad@gmail.com>
2024-03-03 16:46:17 +0330

    fixed bug

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -15,6 +15,7 @@ const formatDate = (date) =>
 function CityItem() {
   const { getCity, currentCity } = useCity();
   const { id } = useParams();
+  const { cityName, emoji, date } = currentCity;
 
   useEffect(() => {
     getCity(id);
@@ -26,18 +27,16 @@ function CityItem() {
     <div className="w-8/12 bg-gray-700 rounded-md p-5 flex flex-col">
       <p className="text-[12px] text-gray-300">CITY NAME</p>
       <p className="mb-8">
-        <span className="text-green-500 text-xl">{currentCity.emoji}</span>
-        <span className="text-xl ml-4 font-medium">{currentCity.cityName}</span>
+        <span className="text-green-500 text-xl">{emoji}</span>
+        <span className="text-xl ml-4 font-medium">{cityName}</span>
       </p>
-      <p className="text-[12px] text-gray-300">
-        YOU WENT TO {currentCity.cityName} ON
-      </p>
-      <p className="mb-8">{formatDate(currentCity.date)}</p>
+      <p className="text-[12px] text-gray-300">YOU WENT TO {cityName} ON</p>
+      {date && <p className="mb-8">{formatDate(date)}</p>}
       <p className="text-[12px] text-gray-300">LEARN MORE</p>
       <Link
         className="underline text-orange-300 mb-8"
         target="_blank"
-        to={`https://en.wikipedia.org/wiki/${currentCity.cityName}`}
+        to={`https://en.wikipedia.org/wiki/${cityName}`}
       >
         Check out Used on Wikipedia →
       </Link>
